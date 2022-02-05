cf7adb5ecd112059d64934689610741593dd4051
CKURZ <ckurz098@gmail.com>
2022-02-05 13:54:28 -0800

    added JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
+
 if (process.env.JAWSDB_URL){
   sequelize =new Sequelize(process.env.JAWSDB_URL);
 } else {
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(
+ sequelize = new Sequelize(
   process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
