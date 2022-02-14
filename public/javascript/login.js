a851577de22fa8efddc52abb074422a539e67d0f
CKURZ <ckurz098@gmail.com>
2022-02-14 13:27:31 -0800

    fixed Auth blocking new user creation

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,15 +3,15 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+console.log(username, email, password);
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
-                username,
-                email,
-                password
+               username: username,
+                email: email,
+                password: password
             }),
             headers: { 'Content-Type': 'application/json' }
         });
